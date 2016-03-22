89745a072ea00998b71bcf6a6603b6b4db4d8d8d
Hideo Doi <dddoooiii888@gmail.com>
2016-03-22 23:06:55 +0900

    renew test

--- wrappers/run_md.sh
+++ wrappers/run_md.sh
@@ -3,9 +3,9 @@ make
 make -f Makefile.testmd
 
 rm 0*
-mpirun.mpich -np 1 ./sample.exe test.inp
+mpirun -np 1 ./sample.exe test.inp
 diff 01_energy.log 05_mpi_ene_00.dat > log.diff1
-mpirun.mpich -np 2 ./sample.exe test.inp
+mpirun -np 2 ./sample.exe test.inp
 diff 01_energy.log 05_mpi_ene_00.dat > log.diff2
 
 cat log.diff1 log.diff2
